73af37928ffc1c713e744a73f85a4630991b18a9
Ahmed Magdy ELzayady <91629766+Ahmed-Elzayady@users.noreply.github.com>
2023-01-26 18:29:49 +0200

    upload

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ icon.onclick = function () {
 }
 
 
-var text = "I'm Ahmed Elzayady , I work as Embedded System Engineer ";
+var text = "I'm Ahmed Elzayady , I work as Embedded System Engineer ..... ";
 var speed = 160; // milliseconds
 var i = 0;
 
@@ -14,17 +14,11 @@ function type() {
       document.getElementById("type-text").innerHTML += text.charAt(i);
       i++;
       setTimeout(type, speed);
-    } else {
-        i = 0;
-        document.getElementById("type-text").innerHTML = "";
-        setTimeout(type, speed);
-    }
+    } 
 }
-
 type();
 
 
-// ////////////////////////////////////////////////////////////////////////////////
 
 var texttwo = "Aliens idea , Human Code";
 var speed2 = 160; // milliseconds
@@ -35,11 +29,9 @@ function typetwo() {
       document.getElementById("type-text2").innerHTML += texttwo.charAt(x);
       x++;
       setTimeout(typetwo, speed2);
-    } else {
-        x = 0;
-        document.getElementById("type-text2").innerHTML = "";
-        setTimeout(typetwo, speed2);
-    }
+    } 
 }
 
 typetwo();
+
+
